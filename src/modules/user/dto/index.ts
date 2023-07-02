22dcd7fbba48cd7ba7d09e2a1a66cc4d9c55084d
Ahmed Hamdy <hamdimn54@gmail.com>
2023-07-02 21:27:56 +0300

    [UserModule] - add find user endpoint - add find all users endpoint - add update user endpoint

---
+++ src/modules/user/dto/index.ts
@@ -0,0 +1,3 @@
+export * from './create-user.dto';
+export * from './find-all-users.dto';
+export * from './update-user.dto';
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+import { CustomResponsePayload } from 'src/shared/response';
 import { CreateUserDto } from './dto/create-user.dto';
+import { FindAllUsersDto } from './dto/find-all-users.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 import { IUserModel, UserModelName } from './schema/user.schema';
 
 @Injectable()
@@ -9,5 +14,26 @@ export class UserService {
 
   async create(createUserDto: CreateUserDto) {
     return this.userModel.create(createUserDto);
+  }
+
+  async findAll({ page, limit, search }: FindAllUsersDto): Promise<CustomResponsePayload<User>> {
+    const [docs, total] = await Promise.all([
+      this.userModel
+        .find({ ...(search && { $text: { $search: search, $caseSensitive: false } }) })
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .sort(...(search ? [{ score: { $meta: 'textScore' as const } }] : ['_id'])),
+      this.userModel.countDocuments({ ...(search && { $text: { $search: search, $caseSensitive: false } }) }),
+    ]);
+
+    return { page, pages: Math.ceil(total / limit), limit, total, data: docs.map((doc) => doc.toObject()) };
+  }
+
+  async findOne(id: Types.ObjectId) {
+    return this.userModel.findById(id);
+  }
+
+  async update(id: Types.ObjectId, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
   }
 }
